737ba3d91cb0d2aa8f7e89ebeccde44ed1a30207
zxhuen <zxhuen324@gmail.com>
2025-07-23 13:43:42 +0800

    Update portfolio content

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -118,7 +118,7 @@ const Projects = () => {
         </motion.div>
 
         <motion.video
-          src="2025-07-22 02-39-59.mp4"
+          src={`${import.meta.env.BASE_URL}2025-07-22 02-39-59.mp4`}
           autoPlay
           loop
           muted
@@ -219,7 +219,9 @@ const Projects = () => {
 
         <motion.div
           className="relative aspect-[3/4] w-full max-w-sm  p-6 rounded-lg shadow-md bg-center bg-cover bg-no-repeat text-white"
-          style={{ backgroundImage: "url('hutato.jpg')" }}
+          style={{
+            backgroundImage: `url(${import.meta.env.BASE_URL}hutato.jpg)`,
+          }}
           initial={{
             y: 80,
             opacity: 0,
